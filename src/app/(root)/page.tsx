66c991680b9be49574b8c81242b08a188be7c68a
novaardiansyah <novaardiansyah78@gmail.com>
2025-02-22 02:20:01 +0700

    Adding a design layout for short URL and QR code generation (currently functioning with dummy data).

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,12 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
-
+import ShortenAndQRCodeTabs from "@/components/ShortenAndQRCodeTabs";
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero  />
+      <ShortenAndQRCodeTabs />
     </>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -68,7 +68,7 @@ export default function Hero() {
       </div>
 
       {/* Hero Content */}
-      <div className="h-screen flex items-center justify-center text-center flex-col px-4 sm:px-36 relative z-10">
+      <div className="h-[80vh] flex items-center justify-center text-center flex-col px-4 sm:px-36 relative z-10">
         <h1 className="sm:text-[48px] text-[30px] font-bold mb-5">Turn clicks into connections</h1>
         <p className="sm:text-[22px] text-[18px]">
           With our advanced URL shortener and QR codes, you can create smarter pathways to your content. <br />
